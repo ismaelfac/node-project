446dbaa1d33a07850d1ccffd0e009680364e7377
ISMAEL E. LASTRE ALVAREZ <ismaelfac@gmail.com>
2021-09-22 21:00:05 -0500

    contract actors controller

--- app/controllers/ContractActorsController.js
+++ app/controllers/ContractActorsController.js
@@ -1,9 +1,14 @@
 const { httpError } = require('../helpers/handleError');
+const mongoose = require('mongoose');
 const ContractActorsSchema  = require('../models/contract_actors')
+const People = require('../models/people');
+const Contract = require('../models/contracts');
+const Actor = require('../models/type_actors');
 
 const index = async (req, res) => {
     try {
         const ListAll = await ContractActorsSchema.find({})
+        
         res.send({ data: ListAll })
     } catch (e) {
         httpError(res, e)
@@ -14,6 +19,9 @@ const getItem = async (req, res) => {
     try {
         const { _id } = req.body;
         const ListAll = await ContractActorsSchema.findById(_id)
+        const result = await Actor.distinct('title', {
+            actorId: mongoose.Types.ObjectId("60edcd88c4546228ece1c37f")
+        });
         res.status(200).send({ data: ListAll })
     } catch (e) {
         httpError(res, e)
@@ -52,8 +60,17 @@ const updatedItem = async (req, res) => {
     }
 }
 
-const deletedItem = (req, res) => {
-    
+const deletedItem = async (req, res) => {
+    try {
+        const { _id } = req.body;
+        const deletedItem = await ContractActorsSchema.findById(_id)
+        if(deletedItem){
+            const result = await ContractActorsSchema.deleteOne({_id})
+            res.status(200).send({ data: result })
+        }        
+    } catch (e) {
+        httpError(res, e)
+    } 
 }
 
 
