912b35c678316c0e610ffb591a4d7d0dde011832
ISMAEL E. LASTRE ALVAREZ <ismaelfac@gmail.com>
2021-09-24 13:51:23 -0500

    contract actors getItem

--- app/controllers/ContractActorsController.js
+++ app/controllers/ContractActorsController.js
@@ -1,9 +1,9 @@
 const { httpError } = require('../helpers/handleError');
 const mongoose = require('mongoose');
 const ContractActorsSchema  = require('../models/contract_actors')
-const People = require('../models/people');
-const Contract = require('../models/contracts');
-const Actor = require('../models/type_actors');
+const people = require('../models/people');
+const contracts = require('../models/contracts');
+const type_actors = require('../models/type_actors');
 
 const index = async (req, res) => {
     try {
@@ -18,11 +18,41 @@ const index = async (req, res) => {
 const getItem = async (req, res) => {
     try {
         const { _id } = req.body;
-        const ListAll = await ContractActorsSchema.findById(_id)
-        const result = await Actor.distinct('title', {
-            actorId: mongoose.Types.ObjectId("60edcd88c4546228ece1c37f")
-        });
-        res.status(200).send({ data: ListAll })
+        const getContractActors = [];
+        const resultContractActors = await ContractActorsSchema.findById(_id)
+        if(!resultContractActors){
+            res.status(404).send("No se encontro registro")
+        }else{
+            const resultTypeActors = await type_actors.find(
+                {_id: resultContractActors.actorId}
+            ).select('nameActor')
+            console.log('resultado actor: '+resultTypeActors[0].nameActor)
+
+            const resultPeople = await people.find(
+                {_id: resultContractActors.peopleId}
+            )
+            console.log('resultado people: '+resultPeople[0].names)
+
+            const resultContract = await contracts.find(
+                { _id: resultContractActors.contractId }
+            ).select('name')
+            console.log('resultado contrato: '+resultContract[0].name)
+            
+            const resultPeopleRepresentante = await people.find(
+                { _id: resultContractActors.PeoplelegalRepresentative }
+            ).select('names')
+            console.log('resultado represante legal: '+resultPeopleRepresentante[0].names)
+            getContractActors.push([
+                { 
+                    "arrendatario": {
+                        "typeActors": resultTypeActors,
+                        "actorsName": resultPeople,
+                    }
+                }
+            ]);
+            console.log('resultado'+getContractActors);
+            res.status(200).send({ data: resultContractActors })
+        }
     } catch (e) {
         httpError(res, e)
     } 
