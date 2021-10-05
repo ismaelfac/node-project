b9ebab5b0184591acb5b0f94e79663f290062481
ISMAEL E. LASTRE ALVAREZ <ismaelfac@gmail.com>
2021-10-05 13:08:50 -0500

    User Controller

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { index, getItem, createdItem, updatedItem, deletedItem } = require('../controllers/UserController');
 const checkOrigin = require('../middleware/origin');
 
-router.get('/', index); //TODO: localhost/users/ ---> lista 
+router.get('/',checkOrigin, index); //TODO: localhost/users/ ---> lista 
 router.get('/:id', getItem); //TODO: localhost/users/:id ---> DETALLE 
 router.post('/', checkOrigin, createdItem); //TODO: localhost/users/ ---> lista 
 router.patch('/:id', updatedItem); //TODO: localhost/users/ ---> lista 
