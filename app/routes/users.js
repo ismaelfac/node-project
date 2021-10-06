da912045af8c2b077655045a2c7b990168a100b4
ISMAEL E. LASTRE ALVAREZ <ismaelfac@gmail.com>
2021-10-06 17:06:12 -0500

    UserController Create Ok

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const { index, getItem, createdItem, updatedItem, deletedItem, activeUser } = require('../controllers/UserController');
 const checkOrigin = require('../middleware/origin');
 
-router.get('/',checkOrigin, index); //TODO: localhost/users/ ---> lista 
+router.get('/', index); //TODO: localhost/users/ ---> lista 
 router.get('/:id', checkOrigin, getItem); //TODO: localhost/users/:id ---> DETALLE 
 router.post('/', checkOrigin, createdItem); //TODO: localhost/users/ ---> lista 
 router.put('/:id', checkOrigin, updatedItem); //TODO: localhost/users/ ---> lista 
-router.delete('/:id', checkOrigin, deletedItem); //TODO: localhost/users/ ---> lista 
-router.post('/:id', checkOrigin, activeUser); //TODO: localhost/users/ ---> lista 
+router.delete('/:id', deletedItem); //TODO: localhost/users/ ---> lista 
+router.post('/:id', activeUser); //TODO: localhost/users/ ---> lista 
 
 module.exports = router;
